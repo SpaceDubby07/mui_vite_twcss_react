e0c38a8ffbc632938ce879908c4e2c59ac26c01f
SpaceDubby07 <zclark006@outlook.com>
2024-09-25 14:56:19 -0400

    dash changes

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -11,7 +11,9 @@ import { useState, useEffect } from 'react';
 
 const Cards = () => {
   const [userCount, setUserCount] = useState<number>(0);
+  const [profileCount, setProfileCount] = useState<number>(0);
   const [postCount, setPostCount] = useState<number>(0);
+  const [commentCount, setCommentCount] = useState<number>(0);
   useEffect(() => {
     const getUsers = async () => {
       try {
@@ -22,6 +24,18 @@ const Cards = () => {
         setUserCount(data.length);
       } catch (error) {
         console.error('Failed to fetch users', error);
+      }
+    };
+
+    const getProfiles = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:3001/api/user_profiles'
+        );
+        const data = await response.json();
+        setProfileCount(data.length);
+      } catch (error) {
+        console.error('Failed to fetch profiles', error);
       }
     };
 
@@ -37,8 +51,22 @@ const Cards = () => {
       }
     };
 
+    const getComments = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:3001/api/comments'
+        );
+        const data = await response.json();
+        setCommentCount(data.length);
+      } catch (error) {
+        console.error('Failed to fetch comments', error);
+      }
+    };
+
+    getComments();
     getPosts();
     getUsers();
+    getProfiles();
   }, []);
 
   return (
@@ -66,10 +94,40 @@ const Cards = () => {
               variant="h5"
               sx={{ color: 'text.secondary' }}
             >
-              Total Users
+              Completed Profiles
+            </Typography>
+
+            <Gauge width={100} height={100} value={profileCount} />
+          </CardContent>
+        </Card>
+      </Stack>
+      <Stack direction={'row'}>
+        <Card>
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              sx={{ color: 'text.secondary' }}
+            >
+              Total Posts
             </Typography>
 
             <Gauge width={100} height={100} value={postCount} />
+          </CardContent>
+        </Card>
+      </Stack>
+      <Stack direction={'row'}>
+        <Card>
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              sx={{ color: 'text.secondary' }}
+            >
+              Total Comments
+            </Typography>
+
+            <Gauge width={100} height={100} value={commentCount} />
           </CardContent>
         </Card>
       </Stack>
